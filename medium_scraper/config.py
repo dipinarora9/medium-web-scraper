f05a959b4eb5922dab6a326eee7beb404e57af10
Dipin Arora <44615649+dipinarora9@users.noreply.github.com>
2022-05-10 17:20:39 +0530

    minor updates

--- medium_scraper/config.py
+++ medium_scraper/config.py
@@ -3,6 +3,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI',
                                              'sqlite:///abc.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from flask_ngrok import run_with_ngrok
 from medium_scraper import create_app
 
 app = create_app()
@@ -7,5 +6,6 @@ if __name__ == '__main__':
     app.debug = True
 
     if not app.debug:
+        from flask_ngrok import run_with_ngrok
         run_with_ngrok(app)
     app.run()
